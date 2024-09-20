dfa43fa0d9802a7dadd2d150622afa709c518553
EvelynDomingues <161346948+EvelynDomingues@users.noreply.github.com>
2024-09-20 15:42:39 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,22 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descrição: "Rayssa Leal, a 'Fadinha', é uma skatista brasileira que conquistou o mundo com seu talento precoce e carisma. Medalhista olímpica e mundial, ela é um dos maiores nomes do skate feminino e um símbolo de inspiração para jovens atletas.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "fada fadinha skate "
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descrição: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros com sua determinação e talento. Bicampeã olímpica, ela é a maior medalhista da história do Brasil nos Jogos Olímpicos, com um currículo invejável que inclui diversas medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: " artística artistica talento"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descrição: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. Com diversas medalhas em competições nacionais e internacionais, ela é uma das principais promessas do judô brasileiro e representa o país em grandes eventos.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: " judo força "
+    }
+];
+
+
